18b321e8756af17e199469da3c07b7b39c96dd86
DevanshuTiwari <devanshu1336@gmail.com>
2025-08-18 18:53:39 +0530

    refactor: add security configuration to allow requests

---
+++ api-gateway/src/main/java/com/dt/api_gateway/config/SecurityConfig.java
@@ -0,0 +1,45 @@
+package com.dt.api_gateway.config;
+
+import io.jsonwebtoken.io.Decoders;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.jwt.NimbusReactiveJwtDecoder;
+import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+import javax.crypto.spec.SecretKeySpec;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+    @Value("${spring.security.oauth2.resourceserver.jwt.secret-key}")
+    private String jwtSecretKey;
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        http
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .authorizeExchange(exchange -> exchange
+                        // Public access to registration and login
+                        .pathMatchers("/api/v1/auth/**", "/api/v1/products/**").permitAll()
+                        .anyExchange().authenticated()
+                )
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
+        return http.build();
+    }
+
+    @Bean
+    public ReactiveJwtDecoder reactiveJwtDecoder() {
+        byte[] keyBytes = Decoders.BASE64.decode(jwtSecretKey);
+        // Create a secret key object from our key string
+        SecretKeySpec secretKey = new SecretKeySpec(keyBytes, "HmacSHA256");
+        // Build the decoder with the secret key
+        return NimbusReactiveJwtDecoder.withSecretKey(secretKey).build();
+    }
+}
+
