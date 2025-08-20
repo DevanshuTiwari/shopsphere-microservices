549d917488f95af4b16b5b794ce4aaf4df9d430e
DevanshuTiwari <devanshu1336@gmail.com>
2025-08-20 16:47:02 +0530

    feat: implement dummy payment service to test circuit breaker pattern

---
+++ payment-service/src/main/java/com/dt/payment_service/controller/PaymentController.java
@@ -0,0 +1,25 @@
+package com.dt.payment_service.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/payments")
+public class PaymentController {
+
+    @PostMapping
+    public ResponseEntity<String> processPayment(@RequestBody Long orderId) {
+
+        System.out.println("Received payment request for order ID: " + orderId);
+        // Randomly succeed or fail
+        if (new java.util.Random().nextInt(10) < 7) { // 70% chance of success
+            return ResponseEntity.ok("Payment successful for order " + orderId);
+        } else {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Payment service is currently unavailable");
+        }
+    }
+}
